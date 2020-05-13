1d4525ca3a86f9daca0b5de3adc5207f9d7b6036
lucasbustoss <lbustos.silva@icloud.com>
2020-05-13 09:58:35 -0300

    Fim do projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  /* Esse primeiro useEffect será executado só uma vez,
+      já que não existe variavel para ser monitorada
+  */
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  /* Esse useEffect será executado toda vez que 'tech'
+      tiver alguma alteração
+  */
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
+  return (
+    <>
+      <ul>
+        {tech.map((t) => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input value={newTech} onChange={(e) => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
